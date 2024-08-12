6d8bf9756011fb2b0ba78068183287d3e567650a
tamirisdepaula <tamiris01gti@gmail.com>
2024-08-12 20:09:33 -0300

    PIZZARIAOK

---
+++ scrips.js
@@ -0,0 +1,10 @@
+let show = true //VARIÁVEL QUE TERÁ O VALOR SEMPRE VERDADEIRO
+const menuContent = document.querySelector('.content')
+const menuToggle = document.querySelector('.menu-toggle')
+
+menuToggle.addEventListener('click', () => {
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+    
+});
